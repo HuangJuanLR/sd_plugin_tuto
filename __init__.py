c3569f531a7ea25baffe8b80593b4ff619f9ee2f
HuangJuanLr <huangjuanlr@gmail.com>
2024-12-19 17:45:49 +0800

    working with pkg_mgr

--- __init__.py
+++ __init__.py
@@ -1,7 +1,40 @@
+import os
+import time
+import sd
+
+context = sd.getContext()
+app = context.getSDApplication()
+
+pkg_mgr = app.getPackageMgr()
+ui_mgr = app.getQtForPythonUIMgr()
+
+# get loaded packages
+all_pkgs = pkg_mgr.getPackages()
+
+for pkg in all_pkgs:
+    print(pkg.getFilePath())
+
+# load packages
+# sbsar_dir = os.path.join("U:/SD/plugins/plugin_tuto/resources", "corrupted_stone_floor.sbsar")
+graph = ui_mgr.getCurrentGraph()
+cur_pkg = graph.getPackage()
+cur_pkg_path = os.path.dirname(cur_pkg.getFilePath())
+sbsar_dir = os.path.join(cur_pkg_path, "resources", "corrupted_stone_floor.sbsar")
+pkg = pkg_mgr.loadUserPackage(sbsar_dir)
+
+# get resource
+res = pkg.findResourceFromUrl("corrupted_stone_floor")
+graph.newInstanceNode(res)
+
+# unload package
+pkg_mgr.unloadUserPackage(pkg)
+
+# create blur node
+blur_node = graph.newNode('sbs::compositing::blur')
+
+
 def initializeSDPlugin():
-    pass
+    print("On SD Plugin Init")
 
 def uninitializeSDPlugin():
-    pass
-
-print("Hello Plugins")
+    print("On SD Plugin Uninit")
