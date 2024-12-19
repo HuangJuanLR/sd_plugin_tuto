0a097e728c14f4a0e97c12c89b01860413935b83
HuangJuanLr <huangjuanlr@gmail.com>
2024-12-19 23:49:09 +0800

    working with ui

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import os
 import sd
 
+from PySide6 import QtCore, QtWidgets, QtGui, QtUiTools
+
 from sd.api.sdproperty import SDPropertyCategory
 from sd.api.sdbasetypes import float2, float4
 from sd.api.sdvaluefloat import SDValueFloat
@@ -13,40 +15,30 @@ pkg_mgr = app.getPackageMgr()
 ui_mgr = app.getQtForPythonUIMgr()
 
 graph = ui_mgr.getCurrentGraph()
+main_window = ui_mgr.getMainWindow()
 
-# get all nodes
-all_nodes = graph.getNodes()
-# get selected nodes
-selected_nodes = ui_mgr.getCurrentGraphSelectedNodes()
+# #creating dialog
+dialog = QtWidgets.QDialog(parent=main_window)
+# dialog.show()
 
-# for n in selected_nodes:
-#     print(n.getDefinition())
-#     print(n.getIdentifier())
-#     print(n.getDefinition().getId())
+# creating menu
+def test_action():
+    # print("Test Action")
+    dialog.show()
 
-selected_props = selected_nodes[0].getProperties(SDPropertyCategory.Output)
+menu_id = "HuangJuanLr" + "#TestMenuItem"
 
-for prop in selected_props:
-    print(prop.getId())
-    print(prop.getType())
+menu = ui_mgr.findMenuFromObjectName(menu_id)
+if menu is not None:
+    ui_mgr.deleteMenu(menu_id)
 
-uniform_color_node = graph.newNode("sbs::compositing::uniform")
-uniform_color_node.newPropertyConnectionFromId("unique_filter_output", selected_nodes[0], "basecolor")
-processor_pos = selected_nodes[0].getPosition()
-uniform_color_node.setPosition(float2(processor_pos.x - 200, processor_pos.y))
-
-
-output_color = uniform_color_node.getPropertyFromId("outputcolor", SDPropertyCategory.Input)
-
-uniform_color_node.setPropertyValue(output_color, SDValueFloat4.sNew(float4(0.5, 0.8, 0.2, 1)))
-
-hue = selected_nodes[0].getPropertyFromId("hue", SDPropertyCategory.Input)
-sat = selected_nodes[0].getPropertyFromId("saturation", SDPropertyCategory.Input)
-light = selected_nodes[0].getPropertyFromId("luminosity", SDPropertyCategory.Input)
-
-selected_nodes[0].setPropertyValue(hue, SDValueFloat.sNew(0.55))
-selected_nodes[0].setPropertyValue(sat, SDValueFloat.sNew(0.525))
-selected_nodes[0].setPropertyValue(light, SDValueFloat.sNew(0.6))
+menu_bar = main_window.menuBar()
+menu = QtWidgets.QMenu("HuangJuanLr", menu_bar)
+menu.setObjectName(menu_id)
+menu_bar.addMenu(menu)
+action = QtGui.QAction("Test", menu)
+action.triggered.connect(test_action)
+menu.addAction(action)
 
 def initializeSDPlugin():
     print("On SD Plugin Init")
