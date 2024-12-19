a35caa98e05da339e71205c3a9717bdc55fd9c7c
HuangJuanLr <huangjuanlr@gmail.com>
2024-12-19 19:56:21 +0800

    working with nodes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 import os
-import time
 import sd
+
+from sd.api.sdproperty import SDPropertyCategory
+from sd.api.sdbasetypes import float2, float4
+from sd.api.sdvaluefloat import SDValueFloat
+from sd.api.sdvaluefloat4 import SDValueFloat4
 
 context = sd.getContext()
 app = context.getSDApplication()
@@ -8,30 +12,41 @@ app = context.getSDApplication()
 pkg_mgr = app.getPackageMgr()
 ui_mgr = app.getQtForPythonUIMgr()
 
-# get loaded packages
-all_pkgs = pkg_mgr.getPackages()
-
-for pkg in all_pkgs:
-    print(pkg.getFilePath())
-
-# load packages
-# sbsar_dir = os.path.join("U:/SD/plugins/plugin_tuto/resources", "corrupted_stone_floor.sbsar")
 graph = ui_mgr.getCurrentGraph()
-cur_pkg = graph.getPackage()
-cur_pkg_path = os.path.dirname(cur_pkg.getFilePath())
-sbsar_dir = os.path.join(cur_pkg_path, "resources", "corrupted_stone_floor.sbsar")
-pkg = pkg_mgr.loadUserPackage(sbsar_dir)
 
-# get resource
-res = pkg.findResourceFromUrl("corrupted_stone_floor")
-graph.newInstanceNode(res)
+# get all nodes
+all_nodes = graph.getNodes()
+# get selected nodes
+selected_nodes = ui_mgr.getCurrentGraphSelectedNodes()
 
-# unload package
-pkg_mgr.unloadUserPackage(pkg)
+# for n in selected_nodes:
+#     print(n.getDefinition())
+#     print(n.getIdentifier())
+#     print(n.getDefinition().getId())
 
-# create blur node
-blur_node = graph.newNode('sbs::compositing::blur')
+selected_props = selected_nodes[0].getProperties(SDPropertyCategory.Output)
 
+for prop in selected_props:
+    print(prop.getId())
+    print(prop.getType())
+
+uniform_color_node = graph.newNode("sbs::compositing::uniform")
+uniform_color_node.newPropertyConnectionFromId("unique_filter_output", selected_nodes[0], "basecolor")
+processor_pos = selected_nodes[0].getPosition()
+uniform_color_node.setPosition(float2(processor_pos.x - 200, processor_pos.y))
+
+
+output_color = uniform_color_node.getPropertyFromId("outputcolor", SDPropertyCategory.Input)
+
+uniform_color_node.setPropertyValue(output_color, SDValueFloat4.sNew(float4(0.5, 0.8, 0.2, 1)))
+
+hue = selected_nodes[0].getPropertyFromId("hue", SDPropertyCategory.Input)
+sat = selected_nodes[0].getPropertyFromId("saturation", SDPropertyCategory.Input)
+light = selected_nodes[0].getPropertyFromId("luminosity", SDPropertyCategory.Input)
+
+selected_nodes[0].setPropertyValue(hue, SDValueFloat.sNew(0.55))
+selected_nodes[0].setPropertyValue(sat, SDValueFloat.sNew(0.525))
+selected_nodes[0].setPropertyValue(light, SDValueFloat.sNew(0.6))
 
 def initializeSDPlugin():
     print("On SD Plugin Init")
