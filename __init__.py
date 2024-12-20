a674598b6e83df5e00ecbbb3d1bac1750c3b5898
HuangJuanLr <huangjuanlr@gmail.com>
2024-12-20 19:01:19 +0800

    create Window.py in lib

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 import os
 import sd
-
 from PySide6 import QtCore, QtWidgets, QtGui, QtUiTools
-
 from sd.api.sdproperty import SDPropertyCategory
 from sd.api.sdbasetypes import float2, float4
 from sd.api.sdvaluefloat import SDValueFloat
 from sd.api.sdvaluefloat4 import SDValueFloat4
+
+from lib import Window
 
 context = sd.getContext()
 app = context.getSDApplication()
@@ -17,16 +17,12 @@ ui_mgr = app.getQtForPythonUIMgr()
 graph = ui_mgr.getCurrentGraph()
 main_window = ui_mgr.getMainWindow()
 
-# #creating dialog
-dialog = QtWidgets.QDialog(parent=main_window)
-# dialog.show()
+win = Window.Window(main_window, pkg_mgr, ui_mgr)
 
-# creating menu
-def test_action():
-    # print("Test Action")
-    dialog.show()
+def show_plugin():
+    win.show()
 
-menu_id = "HuangJuanLr" + "#TestMenuItem"
+menu_id = "HuangJuanLr" + "#BatchProcess"
 
 menu = ui_mgr.findMenuFromObjectName(menu_id)
 if menu is not None:
@@ -36,8 +32,8 @@ menu_bar = main_window.menuBar()
 menu = QtWidgets.QMenu("HuangJuanLr", menu_bar)
 menu.setObjectName(menu_id)
 menu_bar.addMenu(menu)
-action = QtGui.QAction("Test", menu)
-action.triggered.connect(test_action)
+action = QtGui.QAction("Batch Process", menu)
+action.triggered.connect(show_plugin)
 menu.addAction(action)
 
 def initializeSDPlugin():
